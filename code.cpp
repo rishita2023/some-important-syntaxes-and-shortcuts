e57a53a450c8a961a80e7e8601abb8da3867f32e
Rishita Bose <92913222+rishita2023@users.noreply.github.com>
2023-02-10 17:32:51 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,33 @@
+  typedef pair<string, int> pi;
+  
+  
+ /* binary search*/
+  binarySearch(arr, x, low, high)
+        repeat till low = high
+               mid = (low + high)/2
+                   if (x == arr[mid])
+                   return mid
+   
+                   else if (x > arr[mid])
+                       low = mid + 1
+   
+                   else              
+                       high = mid - 1
+/*Recursive Method / divide and conquer method */
+
+    binarySearch(arr, x, low, high)
+           if low > high
+               return False 
+   
+           else
+               mid = (low + high) / 2 
+                   if x == arr[mid]
+                   return mid
+       
+               else if x > arr[mid]       
+                   return binarySearch(arr, x, mid + 1, high)
+               
+               else                       
+                   return binarySearch(arr, x, low, mid - 1) 
+                   
+                   
